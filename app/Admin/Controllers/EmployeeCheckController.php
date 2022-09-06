01aeeb21ff9caa257865ffb5366c937feb2ab624
Tim <lintaitin@gmail.com>
2022-09-06 15:00:30 +0800

    date

--- app/Admin/Controllers/EmployeeCheckController.php
+++ app/Admin/Controllers/EmployeeCheckController.php
@@ -50,7 +50,7 @@ class EmployeeCheckController extends AdminController
             // $grid->column('updated_at')->sortable();
             $grid->filter(function (Grid\Filter $filter) {
                 $filter->equal('id');
-                $filter->between('timestamp');
+                $filter->between('timestamp')->date();
                 $filter->equal('admin_user.name', __('Name'));
             });
         });
