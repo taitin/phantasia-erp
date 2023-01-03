fabd3c7de900f10b2a937519eef520907108c94e
Tim <lintaitin@gmail.com>
2023-01-03 18:17:05 +0800

    expand

---
+++ database/migrations/2023_01_03_181629_add_remain_to_cash_flow_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRemainToCashFlowTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cash_flows', function (Blueprint $table) {
+            $table->integer('remain')->default(0);
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cash_flows', function (Blueprint $table) {
+            //
+        });
+    }
+}
