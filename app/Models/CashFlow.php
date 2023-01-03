f9a4ca56e8570017884fa0ad222d84710041c952
Tim <lintaitin@gmail.com>
2023-01-03 18:18:30 +0800

    updateRemain

--- app/Models/CashFlow.php
+++ app/Models/CashFlow.php
@@ -29,4 +29,51 @@ class CashFlow extends Model
             return Ledger::whereIn('id', $this->ledger_id)->get();
         } else return '';
     }
+
+    public function updateRemain()
+    {
+
+        $cash_flow = CashFlow::where('date',  $this->date)
+            ->where('account_id', $this->account_id)
+            ->where('id', '<', $this->id)
+            ->orderBy('id', 'DESC')
+            ->first();
+        if (empty($cash_flow)) {
+            $cash_flow  =  CashFlow::where('date', '<', $this->date)
+                ->where('account_id', $this->account_id)
+                ->orderBy('date', 'DESC')
+                ->orderBy('id', 'DESC')
+                ->first();
+        }
+        $remain = $cash_flow->remain ?? 0;
+        $org_remain = $this->remain;
+        $this->remain = $remain + $this->income - $this->pay;
+        $this->save();
+
+        if ($org_remain != $this->remain) {
+            $this->updateRelateRemain();
+        }
+    }
+
+    public function updateRelateRemain($id = '', $date = '', $account_id = ' ')
+    {
+        $id = $id ?? $this->id;
+        $date = $date ?? $this->date;
+        $account_id = $account_id ?? $this->account_id;
+
+        $cash_flows =   CashFlow::where('date', '>=', $date)
+            ->where('account_id', $account_id)
+            ->orderBy('date', 'ASC')
+            ->orderBy('id', 'ASC')
+            ->get();
+        foreach ($cash_flows as $cash) {
+            $key = false;
+            if ($cash->date == $date) {
+                if ($cash->id > $id) $key = true;
+            } else {
+                $key = true;
+            }
+            if ($key) $cash->updateRemain();
+        }
+    }
 }
