774695253ffb19d1adf183b7d6305aedc2d2344a
Tim <lintaitin@gmail.com>
2023-07-31 21:26:57 +0800

    calender

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Admin\Metrics\Examples;
 use App\Http\Controllers\Controller;
+use Dcat\Admin\Admin;
 use Dcat\Admin\Http\Controllers\Dashboard;
 use Dcat\Admin\Layout\Column;
 use Dcat\Admin\Layout\Content;
@@ -13,24 +14,21 @@ class HomeController extends Controller
 {
     public function index(Content $content)
     {
+        $user = Admin::user();
+
+        if ($user->inRoles('administrator') || $user->inRoles('accounting')) {
+
+            admin_js([asset('js/fullcalendar-5.10.1/lib/main.min.js'), asset('js/fullcalendar-5.10.1/lib/locales-all.min.js')]);
+            admin_css(asset('js/fullcalendar-5.10.1/lib/main.min.css'));
+
+            return $content
+                ->header('行事曆')
+                ->description('應收應付表')
+                ->body(view('calender'));
+        }
         return $content
-            ->header('Dashboard')
-            ->description('Description...')
-            ->body(function (Row $row) {
-                $row->column(6, function (Column $column) {
-                    $column->row(Dashboard::title());
-                    $column->row(new Examples\Tickets());
-                });
-
-                $row->column(6, function (Column $column) {
-                    $column->row(function (Row $row) {
-                        $row->column(6, new Examples\NewUsers());
-                        $row->column(6, new Examples\NewDevices());
-                    });
-
-                    $column->row(new Examples\Sessions());
-                    $column->row(new Examples\ProductOrders());
-                });
-            });
+            // ->title('主控版面')
+            // ->description('歡迎使用UGO後臺管理系統')
+            ->row(view('title'));
     }
 }
