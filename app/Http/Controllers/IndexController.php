38ea4a3e0a0f195055cf93be612e10924c794659
Tim <lintaitin@gmail.com>
2023-09-20 13:10:15 +0800

    營業報表

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use App\Models\CashFlow;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class IndexController extends Controller
+{
+    //
+
+
+    public function report(Request $request)
+    {
+
+        $date =  $request->date ?? date('Y-m-d');
+        $data['date'] = $date;
+        $yesterday_date  = date('Y-m-d', strtotime("-1 day", strtotime($date)));
+
+        $today_y = date('Y', strtotime($date));
+        $today_m = date('m', strtotime($date));
+        $today_d = date('d', strtotime($date));
+
+        $yesterday_y = date('Y', strtotime("-1 day", strtotime($date)));
+        $yesterday_m = date('m', strtotime("-1 day", strtotime($date)));
+        $yesterday_d = date('d', strtotime("-1 day", strtotime($date)));
+
+        $today =  Http::get("https://shipment.phantasia.com.tw/product_flow/get_order_IO?year=$today_y&mon=$today_m&mday=$today_d&type=json")
+            ->json();
+
+        if ($today_d == '01' || $today_d == 1) $yesterday = [];
+        else $yesterday =  Http::get("https://shipment.phantasia.com.tw/product_flow/get_order_IO?year=$yesterday_y&mon=$yesterday_m&mday=$yesterday_d&type=json")
+            ->json();
+
+
+        //safety check
+
+        $data['sell']['yesterday_in'] =  ($yesterday['orderTotal'] ?? 0) + ($yesterday['ecTotal'] ?? 0);
+        $data['sell']['yesterday_abroad'] = 0; //暫
+
+        $data['sell']['today_in'] = $today['orderTotal'] + $today['ecTotal'] - $data['sell']['yesterday_in'];
+        $data['sell']['today_abroad'] = 0 -   $data['sell']['yesterday_abroad']; //暫時無
+
+        $data['sell']['today_cost'] = -1;
+        $data['sell']['today_profit'] = -1;
+        $data['sell']['yesterday_cost'] = -1;
+        $data['sell']['yesterday_profit'] = -1;
+
+        $data['sell']['total_in'] = $data['sell']['today_in'] + $data['sell']['yesterday_in'];
+        $data['sell']['total_abroad'] =   $data['sell']['today_abroad'] + $data['sell']['yesterday_abroad'];
+
+        $data['sell']['total_cost'] =  $data['sell']['yesterday_cost'] + $data['sell']['today_cost'];
+        $data['sell']['total_profit'] = $data['sell']['yesterday_profit'] + $data['sell']['today_profit'];
+
+        $last_cashs = CashFlow::whereDate('date', '<=', $yesterday_date)
+            ->whereDate('date', '>=', date('Y-m-01', strtotime($yesterday_date)))
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+
+        $today_cashs = CashFlow::whereDate('date', $date)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        $data['charge']['today_in'] =  $today_cashs->sum('income');
+        $data['charge']['today_abroad'] = 0;
+        $data['charge']['yesterday_in'] = $last_cashs->sum('income');
+        $data['charge']['yesterday_abroad'] = 0;
+        $data['charge']['total_in'] = $data['charge']['today_in'] + $data['charge']['yesterday_in'];
+        $data['charge']['total_abroad'] =   $data['charge']['today_abroad'] + $data['charge']['yesterday_abroad'];
+
+
+        $data['receive']['today'] = -1;
+        $data['receive']['yesterday'] = -1;
+        $data['receive']['total'] = -1;
+
+        $data['handle']['today'] = -1;
+        $data['handle']['yesterday'] = -1;
+        $data['handle']['total'] = -1;
+
+        $data['pays'] = [];
+        $data['summary']['today_pay_cash'] = 0;
+        $data['summary']['today_pay_deposit'] = 0;
+        $data['summary']['today_pay_handle'] = 0;
+
+        $data['summary']['last_pay_cash'] = 0;
+        $data['summary']['last_pay_deposit'] = 0;
+        $data['summary']['last_pay_handle'] = 0;
+
+
+        $accounts = Account::all();
+        foreach ($accounts as $account) {
+            $last_cash = CashFlow::where('account_id', $account->id)
+                ->whereDate('date', '<=', $yesterday_date)
+                ->orderBy('created_at', 'desc')
+                ->first();
+            $today_cashs = CashFlow::where('account_id', $account->id)
+                ->whereDate('date', $date)
+                ->orderBy('created_at', 'desc')
+                ->get();
+
+
+            $last_remain =  $last_cash->remain ?? 0;
+            $add = $today_cashs->sum('income') ?? 0;
+            $minus = $today_cashs->sum('pay') ?? 0;
+            $totay_remain =  $today_cashs->first()->remain ?? $last_cash->remain ?? 0;
+
+
+            foreach ($today_cashs as $pay) {
+                if ($pay->pay > 0) {
+                    if ($account->type === 0 || $account->type == "現金") {
+                        $cash = $pay->pay;
+                        $deposit = 0;
+                    } else {
+                        $cash = 0;
+                        $deposit = $pay->pay;
+                    }
+                    $handle = 0;
+                    $data['pays'][] = [
+                        'name' => $pay->comment,
+                        'cash' => $cash,
+                        'deposit' => $deposit,
+                        'handle' => $handle
+                    ];
+
+                    $data['summary']['today_pay_cash'] += $cash;
+                    $data['summary']['today_pay_deposit'] += $deposit;
+                    $data['summary']['today_pay_handle'] += $handle;
+                }
+            }
+
+
+
+            $data['banks'][] = [
+                'name' => $account->name,
+                'last_remain' => $last_remain,
+                'add' =>     $add,
+                'minus' =>   $minus,
+                'totay_remain' => $totay_remain
+            ];
+
+            $account_last_cashes =  $last_cashs->where('account_id', $account->id);
+            //零用金
+            if ($account->type === 0 || $account->type == "現金") {
+                if (!isset($data['summary']['cash'])) $data['summary']['cash'] = [
+                    'last_remain' => 0,
+                    'add' =>  0,
+                    'minus' =>  0,
+                    'totay_remain' => 0,
+                ];
+                $data['summary']['cash']['last_remain'] +=    $last_remain;
+                $data['summary']['cash']['add'] +=  $add;
+                $data['summary']['cash']['minus'] += $minus;
+                $data['summary']['cash']['totay_remain'] +=  $totay_remain;
+                $data['summary']['last_pay_cash'] +=   $account_last_cashes->sum('pay');
+            } else {
+
+                if (!isset($data['summary']['bank'])) $data['summary']['bank'] = [
+                    'last_remain' => 0,
+                    'add' =>  0,
+                    'minus' =>  0,
+                    'totay_remain' => 0,
+                ];
+                $data['summary']['bank']['last_remain'] +=    $last_remain;
+                $data['summary']['bank']['add'] +=  $add;
+                $data['summary']['bank']['minus'] += $minus;
+                $data['summary']['bank']['totay_remain'] +=  $totay_remain;
+                $data['summary']['last_pay_deposit'] +=   $account_last_cashes->sum('pay');
+            }
+        }
+
+
+        for ($i = 0; $i < 15; $i++) {
+            $data['costs'][] = [
+                'name' => '員工薪資',
+                'cash' => 1000,
+                'deposit' => 100,
+                'handle' => 100,
+            ];
+        }
+
+
+
+
+
+        return view('report', $data);
+    }
+}
---
+++ resources/views/report.blade.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>經營日報表</title>
+    <style>
+        table {
+            font-size: 14pt;
+            word-break: keep-all;
+            word-wrap: break-word;
+        }
+
+        table td {
+            height: 20px;
+            text-align: right;
+        }
+
+        .gray {
+            background-color: #D0D0D0;
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <table border="1" style="width: 100%;max-width:800px">
+        <tr>
+            <th colspan="9">
+                <h3>經營日報表<span style="float: right;font-size:12pt">{{ $date }}</span></h3>
+            </th>
+        </tr>
+        <tr>
+            <th colspan="2">科目</th>
+            <th>國內</th>
+            <th>國外</th>
+            <th>小計</th>
+            <th></th>
+            <th>銷貨成本</th>
+            <th>銷貨毛利</th>
+            <th></th>
+        </tr>
+        <tr class="gray">
+            <th rowspan="3">銷貨</th>
+            <th>今日小計</th>
+            <td>{{ number_format($sell['today_in']) }}</td>
+            <td>{{ number_format($sell['today_abroad']) }}</td>
+            <td>{{ number_format($sell['today_in'] + $sell['today_abroad']) }}</td>
+            <td></td>
+            <td>{{ number_format($sell['today_cost']) }}</td>
+            <td>{{ number_format($sell['today_profit']) }}</td>
+            <td></td>
+        </tr>
+        <tr class="gray">
+            <th>昨日累計</th>
+            <td>{{ number_format($sell['yesterday_in']) }}</td>
+            <td>{{ number_format($sell['yesterday_abroad']) }}</td>
+            <td>{{ number_format($sell['yesterday_in'] + $sell['yesterday_abroad']) }}</td>
+            <td></td>
+            <td>{{ number_format($sell['yesterday_cost']) }}</td>
+            <td>{{ number_format($sell['yesterday_profit']) }}</td>
+            <td></td>
+        </tr>
+        <tr class="gray">
+            <th>今日累計</th>
+            <td>{{ number_format($sell['today_in']) }}</td>
+            <td>{{ number_format($sell['today_abroad']) }}</td>
+            <td>{{ number_format($sell['today_in'] + $sell['today_abroad']) }}</td>
+            <td></td>
+            <td>{{ number_format($sell['total_cost']) }}</td>
+            <td>{{ number_format($sell['total_profit']) }}</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <th rowspan="3">收款</th>
+            <th>今日小計</th>
+            <td>{{ number_format($charge['today_in']) }}</td>
+            <td>{{ number_format($charge['today_abroad']) }}</td>
+            <td>{{ number_format($charge['today_in'] + $charge['today_abroad']) }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th>昨日累計</th>
+            <td>{{ number_format($charge['yesterday_in']) }}</td>
+            <td>{{ number_format($charge['yesterday_abroad']) }}</td>
+            <td>{{ number_format($charge['yesterday_in'] + $charge['yesterday_abroad']) }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th>今日累計</th>
+            <td>{{ number_format($charge['total_in']) }}</td>
+            <td>{{ number_format($charge['total_abroad']) }}</td>
+            <td>{{ number_format($charge['total_in'] + $charge['total_abroad']) }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr class="gray">
+            <th>應收帳款</th>
+            <td></td>
+            <td>{{ number_format($receive['today']) }}</td>
+            <td>{{ number_format($receive['yesterday']) }}</td>
+            <td>{{ number_format($receive['total']) }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <th>應付帳款</th>
+            <td></td>
+            <td>{{ number_format($handle['today']) }}</td>
+            <td>{{ number_format($handle['yesterday']) }}</td>
+            <td>{{ number_format($handle['total']) }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="9"></td>
+        </tr>
+        <tr>
+            <th>銀行別存款</th>
+            <th>昨日餘額</th>
+            <th>增加</th>
+            <th>減少</th>
+            <th>今日餘額</th>
+            <th>科目</th>
+            <th>零用金</th>
+            <th>銀行存款</th>
+            <th>應付款</th>
+
+        </tr>
+
+        @for ($key = 0; $key < max(count($banks), count($pays)); $key++)
+            <tr>
+                <th>{{ $banks[$key]['name'] ?? '' }}</th>
+                <td>{{ isset($banks[$key]['last_remain']) ? number_format($banks[$key]['last_remain']) : '' }}</td>
+                <td>{{ isset($banks[$key]['add']) ? number_format($banks[$key]['add']) : '' }}</td>
+                <td>{{ isset($banks[$key]['minus']) ? number_format($banks[$key]['minus']) : '' }}</td>
+                <td>{{ isset($banks[$key]['totay_remain']) ? number_format($banks[$key]['totay_remain']) : '' }}</td>
+                @if (isset($pays[$key]))
+                    <th class="gray">{{ $pays[$key]['name'] ?? '' }}</th>
+                    <td class="gray">{{ number_format($pays[$key]['cash'] ?? 0) }}</td>
+                    <td class="gray">{{ number_format($pays[$key]['deposit'] ?? 0) }}</td>
+                    <td class="gray">{{ number_format($pays[$key]['handle'] ?? 0) }}</td>
+                @else
+                    <th class="gray"></th>
+                    <td class="gray"></td>
+                    <td class="gray"></td>
+                    <td class="gray"></td>
+                @endif
+            </tr>
+        @endfor
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="gray"></td>
+            <td class="gray"></td>
+            <td class="gray"></td>
+            <td class="gray"></td>
+
+        </tr>
+        <tr>
+            <th class="gray">銀行定存</th>
+            <td class="gray"></td>
+            <td class="gray"></td>
+            <td class="gray"></td>
+            <td class="gray"></td>
+            <th>今日小計</th>
+            <td>{{ number_format($summary['today_pay_cash'] ?? 0) }}</td>
+            <td>{{ number_format($summary['today_pay_deposit'] ?? 0) }}</td>
+            <td>{{ number_format($summary['today_pay_handle'] ?? 0) }}</td>
+        </tr>
+        <tr>
+            <th class="gray">零用金</th>
+            <td class="gray">{{ number_format($summary['cash']['last_remain'] ?? 0) }}</td>
+            <td class="gray">{{ number_format($summary['cash']['add'] ?? 0) }}</td>
+            <td class="gray">{{ number_format($summary['cash']['minus'] ?? 0) }}</td>
+            <td class="gray">{{ number_format($summary['cash']['totay_remain'] ?? 0) }}</td>
+            <th>昨日累計</th>
+            <td>{{ number_format($summary['last_pay_cash'] ?? 0) }}</td>
+            <td>{{ number_format($summary['last_pay_deposit'] ?? 0) }}</td>
+            <td>{{ number_format($summary['last_pay_handle'] ?? 0) }}</td>
+        </tr>
+        <tr>
+            <th class="gray">手存現金/票據</th>
+            <td class="gray">{{ number_format($summary['bank']['last_remain'] ?? 0) }}</td>
+            <td class="gray">{{ number_format($summary['bank']['add'] ?? 0) }}</td>
+            <td class="gray">{{ number_format($summary['bank']['minus'] ?? 0) }}</td>
+            <td class="gray">{{ number_format($summary['bank']['totay_remain'] ?? 0) }}</td>
+            <th>今日累計</th>
+            <td>{{ number_format($summary['last_pay_cash'] + $summary['today_pay_cash']) }}</td>
+            <td>{{ number_format($summary['last_pay_deposit'] + $summary['today_pay_deposit']) }}</td>
+            <td>{{ number_format($summary['last_pay_handle'] + $summary['today_pay_handle']) }}</td>
+        </tr>
+        <tr>
+
+
+        <tr>
+            <th>合計</th>
+            <td>{{ number_format($summary['cash']['last_remain'] + $summary['bank']['last_remain']) }}</td>
+            <td>{{ number_format($summary['cash']['add'] + $summary['bank']['add']) }}</td>
+            <td>{{ number_format($summary['cash']['minus'] + $summary['bank']['minus']) }}</td>
+            <td>{{ number_format($summary['cash']['totay_remain'] + $summary['bank']['totay_remain']) }}</td>
+            <th></th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+    </table>
+
+
+
+
+</body>
+
+</html>
