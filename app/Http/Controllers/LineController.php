fee5cbbba2d974f418615ba9c4214fa15a3f5d47
Tim <lintaitin@gmail.com>
2023-09-24 12:01:13 +0800

    request

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -12,6 +12,8 @@ class LineController extends Controller
     public function LineCallback(Request $request)
     {
 
+        Log::debug($request);
+        return;
         $replyToken = $request->events[0]['replyToken'];
         $inputText = $request->events[0]['message']['text'] ?? '';
         $socialId = $request->events[0]['source']['userId'];
