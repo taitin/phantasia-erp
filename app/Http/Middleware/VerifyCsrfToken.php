2ac6dcb60385089f935656d317cecc5840c39c7d
Tim <lintaitin@gmail.com>
2023-12-04 15:44:28 +0800

    VerifyCsrfToken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/line/callback'
+        '/line/callback',
+        '/api/*'
+
         //
     ];
 }
