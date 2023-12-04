fa3fae398c68a78ebde175da3c2da15a1030b480
Tim <lintaitin@gmail.com>
2023-12-04 20:26:37 +0800

    dump($root->tagName);

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -26,7 +26,7 @@ class ApiController extends Controller
     public function ky_order(Request $request)
     {
 
-
+        $orderNum = time();
         $data = [
             'APIKey' => config('ky.APIKey'),
             'cusID' => config('ky.cusID'),
@@ -35,18 +35,18 @@ class ApiController extends Controller
             [
                 'main' =>
                 [
-                    'serviceType' => 3, //服務類別 1:取貨付款(店配) / 代收貨款(宅配) 3:取貨不付款(店配) / 一般配送(宅配、海外)
-                    'orderID' => 'AT20230810111452', //訂單號碼
-                    'shippingID' => 'BT20230810111452', //出貨單號 若無出貨單號，請填入訂單號碼
+                    'serviceType' => 1, //服務類別 1:取貨付款(店配) / 代收貨款(宅配) 3:取貨不付款(店配) / 一般配送(宅配、海外)
+                    'orderID' =>    $orderNum, //訂單號碼
+                    'shippingID' =>    $orderNum, //出貨單號 若無出貨單號，請填入訂單號碼
                     'orderAmount' => '999', //代收金額 或包裹價值
                     'memberID' => 'M001', //會員編號
                     'memberName' => '王小姐', //會員名稱
                     'rvName' => '王姑娘', //取件人姓名
                     'rvZip' => '', //取件人郵遞區號
-                    'rvAddr' => '', //取件人地址
-                    'rvDTel' => '', //取件人日間電話
-                    'rvNTel' => '', //取件人夜間電話
-                    'rvMobile' => '', //取件人行動電話
+                    'rvAddr' => '新北市板橋區南雅南路二段11-26號', //取件人地址
+                    'rvDTel' => '0921181465', //取件人日間電話
+                    'rvNTel' => '0921181465', //取件人夜間電話
+                    'rvMobile' => '0921181465', //取件人行動電話
                     'stType' => '', //取件門市通路  S11:統一超商 F:全家
                     'stCode' => '', //取件門市代碼
                     'stName' => '', //取件門市名稱
