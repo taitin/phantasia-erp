24d46e63cf8d669338e12e71860ada0652418b31
Tim <lintaitin@gmail.com>
2023-12-08 16:53:39 +0800

    已成單送到凱耀物流，點選確認後關閉

--- resources/views/xml_loading.blade.php
+++ resources/views/xml_loading.blade.php
@@ -13,7 +13,7 @@
         <h3>已成單送到凱耀物流，點選確認後關閉</h3>
     @else
         <h3>傳送中....<img src="/images/loading.gif"></h3>
-        <iframe src="{{ url('api/go_ky_order?shipmentID=' . $shipmentID) }}"></iframe>;
+        <iframe border=0 src="{{ url('api/go_ky_order?shipmentID=' . $shipmentID) }}"></iframe>;
     @endif
 
 
