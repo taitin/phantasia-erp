c01b585c8cc21e473b44628aaed8008797207164
Tim <lintaitin@gmail.com>
2023-12-08 17:02:10 +0800

    正在傳送中到凱耀物流中

--- resources/views/xml_loading.blade.php
+++ resources/views/xml_loading.blade.php
@@ -18,7 +18,7 @@
         <h3>已成單送到凱耀物流，點選確認後關閉</h3>
     @else
         <h3>正在傳送中到凱耀物流中....<img src="/images/loading.gif"></h3>
-        <iframe border=0 src="{{ url('api/go_ky_order?shipmentID=' . $shipmentID) }}"></iframe>;
+        <iframe style="width: 100%" border=0 src="{{ url('api/go_ky_order?shipmentID=' . $shipmentID) }}"></iframe>;
     @endif
 
 
