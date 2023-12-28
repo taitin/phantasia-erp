0ceaa03f1971760d0995626d26dfb2ff79c08545
Tim <lintaitin@gmail.com>
2023-12-28 11:56:19 +0800

    2bf3b310eed75465b22b9d2a2c939654

--- config/ky.php
+++ config/ky.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    // 'APIKey' => '2bf3b310eed75465b22b9d2a2c939654',
-    // 'cusID' => 77011,
-    'APIKey' => '57751b84f00f05dfc1734ee9714d1f68',
-    'cusID' => '04092',
+    'APIKey' => '2bf3b310eed75465b22b9d2a2c939654',
+    'cusID' => 77011,
+    // 'APIKey' => '57751b84f00f05dfc1734ee9714d1f68',
+    // 'cusID' => '04092',
     'url' => 'https://ky.astertech.com.tw/order_api/ky_order_api.jsp',
     'cusName' => '幻遊天下股份有限公司',
     'attributes' => [
